e795c5660bc4969a73e86a0eb7bbfb285a941528
Jonathan Brown <jbrown@bluedroplet.com>
2017-01-04 22:00:01 +0700

    Use provided limit instead of hardcoded.

--- mainstreet_crowdfund.sol
+++ mainstreet_crowdfund.sol
@@ -29,7 +29,7 @@ contract MainstreetCrowdfund {
         if (block.timestamp < start || block.timestamp >= end) {
             throw;
         }
-        if (totalMIT >= 50000000 ether) {    // ether is just a multiplier
+        if (totalMIT >= limit) {    // ether is just a multiplier
             throw;
         }
         _;
