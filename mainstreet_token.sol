b4eeb8d0e88d0e69ef77a6c9da87a01c4c501824
Jonathan Brown <jbrown@bluedroplet.com>
2017-01-13 20:42:08 +0700

    Mitigate approve() race condition.

--- mainstreet_token.sol
+++ mainstreet_token.sol
@@ -124,6 +124,14 @@ contract MainstreetToken is ERC20 {
      * @dev Implements ERC20 approve()
      */
     function approve(address _spender, uint256 _value) isActive returns (bool success) {
+        // To change the approve amount you first have to reduce the addresses´
+        // allowance to zero by calling `approve(_spender,0)` if it is not
+        // already 0 to mitigate the race condition described here:
+        // https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+        if ((_value != 0) && (allowed[msg.sender][_spender] != 0)) {
+            throw;
+        }
+
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
